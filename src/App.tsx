f6c524e3de80c47d27a1aeff903105d9d116188a
shresth-sam-004 <shresth-sam-004@users.noreply.github.com>
2026-04-23 01:06:54 +0530

    Clean up unused components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Work from './pages/Work';
-import Links from './pages/Links';
-import Uses from './pages/Uses';
-import Achievements from './pages/Achievements';
-
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
@@ -15,11 +9,6 @@ function App() {
       {/* Route mapping */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/links" element={<Links />} />
-        <Route path="/uses" element={<Uses />} />
-        <Route path="/achievements" element={<Achievements />} />
       </Routes>
     </>
   );
